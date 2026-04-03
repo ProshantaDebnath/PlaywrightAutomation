35a7ce32a720bd9eceeb68db2e90785a47ee341d
Proshanta Debnath <debnathproshanta80@gmail.com>
2026-04-04 02:28:31 +0530

    Added all the browser plugins & Project setup etc

---
+++ tests/UIBasic.spec.js
@@ -0,0 +1,38 @@
+const assert = require('assert')
+const { test, expect } = require('@playwright/test')
+
+
+test('@Web Client App login', async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const userName = page.locator("#username")
+    const password = page.locator("#password")
+    const signInbutton = page.locator("[name='signin']")
+    const navBar = page.locator('text="ProtoCommerce Home"');
+    const cardsName = page.locator('.card-body .card-title a')
+
+    await userName.fill("rahulshettyacademy");
+    await password.fill("Learning@830$3mK2");
+    await signInbutton.click();
+    await expect(navBar).toContainText("ProtoCommerce")
+
+    const expectedProducts = [
+        'iphone X',
+        'Samsung Note 8',
+        'Nokia Edge',
+        'Blackberry'
+    ]
+
+    const actualProducts = await cardsName.allTextContents()
+    const message = `Expected products: ${JSON.stringify(expectedProducts)}, Actual products: ${JSON.stringify(actualProducts)}`
+    console.log(message)
+    assert.deepStrictEqual(actualProducts, expectedProducts, message)
+
+
+
+
+
+
+
+})
